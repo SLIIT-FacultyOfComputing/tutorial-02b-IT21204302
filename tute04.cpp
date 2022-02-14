31acca0a75522671e01bddb16d013dd27b38c1ff
IT21204302 <99412150+IT21204302@users.noreply.github.com>
2022-02-14 13:50:14 +0000

    completed all the questions

--- tute04.cpp
+++ tute04.cpp
@@ -16,14 +16,53 @@ Do not modify the main function.*/
 long Factorial(int no);
 long nCr(int n, int r);
 
-int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+int main() 
+{
+	int n, r;
+	std::cout << "Enter a value for n ";
+	std::cin >> n;
+	std::cout << "Enter a value for r ";
+	std::cin >> r;
+	std::cout << "nCr = ";
+	std::cout << nCr(n, r);
+	std::cout << std::endl;
+}
+
+long Factorial(int no)
+{
+	long fac;
+	int  r;
+	fac = 1;
+	for (r = no; r >= 1; r--)
+	{
+		fac = fac * r;
+	}
+	return fac;
+}
+
+long nCr(int n, int r)
+{
+	long facN, facR, facNR, nCr;
+	int  x;
+	facN = 1;
+	for (x = n; x >= 1; x--)
+	{
+		facN = facN * x;
+	}
+
+	facR = 1;
+	for (x = r; x >= 1; x--)
+	{
+		facR = facR * x;
+	}
+
+	facNR = 1;
+	for (x = (n - r); x >= 1; x--)
+	{
+		facNR = facNR * x;
+	}
+
+	nCr = facN / (facR * facNR);
+
+	return nCr;
 }
